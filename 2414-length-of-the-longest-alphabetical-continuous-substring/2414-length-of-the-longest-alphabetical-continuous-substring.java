8af00b80f60a0431f5328df8864410241feba600
Nischay Raj <nischayraval9@gmail.com>
2025-12-17 14:47:46 +0530

    Time: 14 ms (17.89%), Space: 47.6 MB (26.84%) - LeetHub

--- 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.java
+++ 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.java
@@ -4,7 +4,7 @@ class Solution {
         int max = 0 ;
         for(int i = 0 ; i < s.length()-1; i++){
 
-            if(s.charAt(i)  == (char)(s.charAt(i+1) - 1)){
+            if(s.charAt(i) - 'a' == (s.charAt(i+1) - 'a') - 1){
                        cnt++;
             } 
             else{
